e68afb8ea7b7ae42eb9375a31a92d58d325f3d65
noahlissner <noah.lissner@gmail.com>
2022-04-28 13:51:13 +0200

    Light/dark switch animation

--- src/components/Switcher.js
+++ src/components/Switcher.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import useDarkSide from "../hooks/useDarkSide";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 // import { RiMoonClearFill, RiSunFill } from "react-icons/ri";
 
 import { ReactComponent as MoonIcon } from "../assets/icon-moon.svg";
@@ -9,12 +9,12 @@ import { ReactComponent as SunIcon } from "../assets/icon-sun.svg";
 export default function Switcher() {
   const [colorTheme, setTheme] = useDarkSide();
   // const [darkSide, setDarkSide] = useState(colorTheme === "light" ? true : false)
-  const [isOn, setIsOn] = useState(false);
+  const [isOn, setIsOn] = useState(true);
 
   // const toggleSwitch = () => setIsOn(!isOn);
 
   const toggleDarkMode = () => {
-    setIsOn(!isOn);
+    setIsOn(colorTheme === "light" ? false : true);
     setTheme(colorTheme);
   };
 
@@ -35,23 +35,22 @@ export default function Switcher() {
 
     <div className="" onClick={toggleDarkMode}>
       {isOn ? (
-        <motion.div
-          initial={{ scale: 0.9 }}
-          animate={{ scale: 1.0 }}
-          className="flex gap-4 items-center cursor-pointer text-darkTheme-text hover:text-darkTheme-switch-hover font-bold duration-75"
-        >
-          <p className="">Light</p>
-          <SunIcon />
-        </motion.div>
+        <div className="flex gap-4 items-center cursor-pointer text-darkTheme-text hover:text-darkTheme-switch-hover font-bold duration-75">
+          <p>Light</p>
+          <AnimatePresence>
+            <motion.div initial={{ y: -20 }} animate={{ y: 0 }}>
+              <SunIcon />
+            </motion.div>
+          </AnimatePresence>
+        </div>
       ) : (
-        <motion.div
-          initial={{ scale: 0.9 }}
-          animate={{ scale: 1.0 }}
-          className="flex gap-4 items-center cursor-pointer text-lightTheme-switch hover:text-lightTheme-logo font-bold duration-75"
-        >
-          <p className="">Dark</p>
-          <MoonIcon className="" />
-        </motion.div>
+        <div className="flex gap-4 items-center cursor-pointer text-lightTheme-switch hover:text-lightTheme-logo font-bold duration-75">
+          <p>Dark</p>
+
+          <motion.div initial={{ y: -20 }} animate={{ y: 0 }}>
+            <MoonIcon />
+          </motion.div>
+        </div>
       )}
     </div>
   );
